b6177b6ab829e9aeb25d42c78e0f8a48f3498945
Timu5 <timusziomek@gmail.com>
2021-04-01 11:00:15 +0200

    Add main

---
+++ zad2/main.py
@@ -0,0 +1,53 @@
+from antlr4 import *
+from ctypes import CFUNCTYPE, c_double, c_int
+from generated.CalcLexer import CalcLexer
+from generated.CalcParser import CalcParser
+
+from Codegen import *
+import llvmlite.binding as llvm
+from antlr4.tree.Trees import Trees
+
+def exec(module):
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()
+
+    target = llvm.Target.from_default_triple()
+    target_machine = target.create_target_machine()
+
+    backing_mod = llvm.parse_assembly("")
+    engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
+
+    mod = llvm.parse_assembly(str(module))
+    engine.add_module(mod)
+    engine.finalize_object()
+    engine.run_static_constructors()
+
+    func_ptr = engine.get_function_address("start")
+
+    cFunc = CFUNCTYPE(None)(func_ptr)
+    cFunc()
+
+def magic(txt):
+    lexer = CalcLexer(InputStream(txt))
+    stream = CommonTokenStream(lexer)
+    parser = CalcParser(stream)
+    tree = parser.program()
+
+    print(Trees.toStringTree(tree, None, parser))
+
+    print()
+
+    codegen = Codegen()
+    ir = codegen.gen_ir(tree)
+    print(str(ir))
+    exec(ir)
+
+
+f = open("test.pclang", "r")
+txt = f.read()
+magic(txt)
+'''
+while(True):
+    print(">>>", end="")
+    magic(input())'''
