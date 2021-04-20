1081c99b22a8431b5ba63ac7aa90a0f17309744b
Timu5 <timusziomek@gmail.com>
2021-04-20 22:09:07 +0200

    Add utils

--- zad2/Codegen.py
+++ zad2/Codegen.py
@@ -3,23 +3,8 @@ from antlr4 import *
 from generated.LangLexer import LangLexer
 from generated.LangVisitor import LangVisitor
 from generated.LangParser import LangParser
-import re
+from Utils import *
 
-
-class CodegenException(Exception):
-    def __init__(self, start, msg):
-        self.line = start.line
-        self.column = start.column
-        self.msg = msg
-        super().__init__(self.msg)
-
-class SignedType(ir.IntType):
-    def __new__(cls, value, *args, **kwargs):
-        return ir.IntType.__new__(cls, value)
-
-    def __init__(self, size, signed):
-        self.is_signed = signed
-        self.is_unsigned = not signed
 
 class Codegen(LangVisitor):
 
